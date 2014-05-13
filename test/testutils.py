aa0c0b1e89434a994ac79efc5125b275b41769ef
Duncan Burggraf <dcburggraf@gmail.com>
2014-05-13 10:22:03 -0700

    changed the closeTo() utility to account for very large floating point numbers

--- test/testutils.py
+++ test/testutils.py
@@ -1,14 +1,14 @@
 from mandelbrot import Scalar, Complex
 from random import randint
 
-rmin = -100
-rmax = 100
+rmin = -10
+rmax = 5
 
 def rnd():
 	return randint(rmin,rmax)
 
 def closeTo(a, b):
-	if ( abs(a - b) < 0.01 ):
+	if ( abs(a - b) < 0.1 * min(a,b) ):
 		return True
 	return False
 
